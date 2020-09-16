68670bc6c42c38b5ea4ec2b1487f3e602ee3a1e0
chabe-bit <62448035+chabe-bit@users.noreply.github.com>
2020-09-15 18:28:56 -0700

    Create Sealion.h

---
+++ Sealion.h
@@ -0,0 +1,11 @@
+#ifndef SEALION_H
+#define SEALION_H
+#include "animal.h"
+
+class Sealion : public Animal {
+   public:
+      Sealion();
+      Sealion(int,int,int,int,int);
+      ~Sealion();
+};
+#endif
